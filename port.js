95cd94ca92f7dce44ebec27e16574620ab9967e1
Manan Batra <mananbatra162004@gmail.com>
2024-07-27 02:27:14 +0530

    Updated Code

--- port.js
+++ port.js
@@ -6,6 +6,11 @@ let card = document.getElementsByClassName("card")
 let twitterlogo = document.getElementsByClassName("twitter")
 let contact = document.getElementById("Contact")
 let form = document.getElementsByClassName("form")
+let borderline = document.getElementsByClassName("borderline")
+let bubble = document.getElementsByClassName("bubble7")
+let bubble1 = document.getElementsByClassName("bubble8")
+
+
 
 dark[0].addEventListener("click",()=>{
     menu[0].classList.toggle("dark-mode");
@@ -18,11 +23,19 @@ dark[0].addEventListener("click",()=>{
 })
 
 contact.addEventListener("click",()=>{
-   if(form[0].style.display==='block'){
-    form[0].style.display = 'none';
+   if(form[0].style.display==='none'){
+    form[0].style.display = 'flex';
+    borderline[0].style.display = 'none';
+    content[0].style.display = 'none';
+    bubble[0].style.display = 'none';
+    bubble1[0].style.display = 'none';
    }
    else{
-    form[0].style.display = 'block';
+    form[0].style.display = 'none';
+    borderline[0].style.display = 'flex';
+    content[0].style.display = 'flex';
+    bubble[0].style.display = 'flex';
+    bubble1[0].style.display = 'flex';
    }
 })
 
